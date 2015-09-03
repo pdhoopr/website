c0f16b2d8d98ee038c3b4dba5155fea413663294
Patrick Hooper <patrick.d.hooper@gmail.com>
2015-09-03 15:28:36 -0400

    Updated hero animation timing

--- js/src/patrick.hoopr.js
+++ js/src/patrick.hoopr.js
@@ -207,9 +207,9 @@ if (wrapper.hasClass('layout-home')) {
 /* Scripts
    ======================================================================== */
 
-/* Tell ScrollMagic to animate scroll over 0.5 sec rather than jump */
+/* Tell ScrollMagic to animate scroll over 0.6 sec rather than jump */
 scrollMagicController.scrollTo(function (newpos) {
-  TweenMax.to(window, 0.5, {scrollTo: {y: newpos}});
+  TweenMax.to(window, 0.6, {scrollTo: {y: newpos}});
 });
 
 /* Tell ScrollMagic to listen for anchor link clicks and scroll to them */
@@ -237,7 +237,7 @@ if (wrapper.hasClass('layout-home')) {
     taglines.hide();
 
     /* Start cycling through taglines 1 seconds after page ready */
-    setTimeout(cycle, 500);
+    setTimeout(cycle, 700);
 
     /* Bring Learn More button to full opacity 8 seconds after page ready */
     setTimeout(function() {
