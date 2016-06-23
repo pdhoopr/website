74bfc373d762ef6a559ddb30a7734b3ada51e1e3
Patrick Hooper <patrick.d.hooper@gmail.com>
2016-06-23 12:37:08 -0400

    Install vendor deps through npm, update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -27,7 +27,7 @@ const util = require('gulp-util');
 const env = process.env.NODE_ENV || 'development';
 
 /* Set path objects used in locating and compiling assets */
-const paths = {
+const tasks = {
   docs: {
     watchFiles: 'docs/**',
     src: 'docs/**',
@@ -74,43 +74,59 @@ const paths = {
   },
   css: {
     watchFiles: [
-      'vendor/assets/css/sassy-maps/**',
-      'vendor/assets/css/breakpoint/**',
       'app/_assets/css/**',
+      'node_modules/breakpoint-sass/stylesheets/**',
+      'node_modules/sassy-maps/sass/**',
+      'node_modules/unsemantic/assets/sass/**',
     ],
     src: 'app/_assets/css/main.scss',
     dest: 'public/css',
+    sass: {
+      includePaths: [
+        'node_modules/breakpoint-sass/stylesheets',
+        'node_modules/sassy-maps/sass',
+        'node_modules/unsemantic/assets/sass',
+      ],
+    },
   },
   vendor: {
     fonts: {
-      watchFiles: 'vendor/assets/fonts/**',
-      src: 'vendor/assets/fonts/**',
+      watchFiles: 'node_modules/font-awesome/fonts/**',
+      src: 'node_modules/font-awesome/fonts/**',
       dest: 'public/fonts',
     },
     js: {
-      watchFiles: 'vendor/assets/js/**',
+      watchFiles: [
+        'app/_assets/vendor/modernizr/modernizr.min.js',
+        'node_modules/babel-polyfill/dist/polyfill.min.js',
+        'node_modules/gsap/src/minified/jquery.gsap.min.js',
+        'node_modules/gsap/src/minified/plugins/ScrollToPlugin.min.js',
+        'node_modules/gsap/src/minified/TweenMax.min.js',
+        'node_modules/jquery/dist/jquery.min.js',
+        'node_modules/scrollmagic/scrollmagic/minified/plugins/animation.gsap.min.js',
+        'node_modules/scrollmagic/scrollmagic/minified/ScrollMagic.min.js',
+        'node_modules/typed.js/dist/typed.min.js',
+        'node_modules/unveil2/dist/jquery.unveil2.min.js',
+        'node_modules/waypoints/lib/jquery.waypoints.min.js',
+      ],
       src: [
-        'vendor/assets/js/modernizr/modernizr.min.js',
-        'vendor/assets/js/jquery/jquery.js',
-        'vendor/assets/js/gsap/TweenMax.js',
-        'vendor/assets/js/gsap/jquery.gsap.js',
-        'vendor/assets/js/gsap/ScrollToPlugin.js',
-        'vendor/assets/js/scrollmagic/ScrollMagic.js',
-        'vendor/assets/js/scrollmagic/animation.gsap.js',
-        'vendor/assets/js/waypoints/jquery.waypoints.js',
-        'vendor/assets/js/typedjs/typed.js',
-        'vendor/assets/js/unveil/jquery.unveil.js',
-        'vendor/assets/js/babel/polyfill.js',
+        'app/_assets/vendor/modernizr/modernizr.min.js',
+        'node_modules/babel-polyfill/dist/polyfill.js',
+        'node_modules/jquery/dist/jquery.js',
+        'node_modules/gsap/src/minified/TweenMax.min.js',
+        'node_modules/gsap/src/minified/jquery.gsap.min.js',
+        'node_modules/gsap/src/minified/plugins/ScrollToPlugin.min.js',
+        'node_modules/typed.js/dist/typed.min.js',
+        'node_modules/scrollmagic/scrollmagic/minified/ScrollMagic.min.js',
+        'node_modules/scrollmagic/scrollmagic/minified/plugins/animation.gsap.min.js',
+        'node_modules/waypoints/lib/jquery.waypoints.min.js',
+        'node_modules/unveil2/dist/jquery.unveil2.min.js',
       ],
       dest: 'public/js',
     },
     css: {
-      watchFiles: [
-        'vendor/assets/css/**',
-        '!vendor/assets/css/sassy-maps/**',
-        '!vendor/assets/css/breakpoint/**',
-      ],
-      src: 'vendor/assets/css/vendor.scss',
+      watchFiles: ['node_modules/font-awesome/scss/**', 'node_modules/reset-css/reset.css'],
+      src: ['node_modules/reset-css/reset.css', 'node_modules/font-awesome/scss/font-awesome.scss'],
       dest: 'public/css',
     },
   },
@@ -128,18 +144,63 @@ function buildErrorMessage(task) {
    ========================================================================= */
 
 /**
+ * Stylesheets Task
+ *
+ * 1. Locates the src of css files specified in tasks object
+ * 2. Initializes sourcemaps
+ * 3. Minifies the file if this is a production run, otherwise leaves expanded
+ * 4. Uses Autoprefixer to add vendor prefixes
+ * 5. Renames file to style.css
+ * 6. Writes the file to the css destination specified in the tasks object w/ sourcemap
+ */
+gulp.task('css', () =>
+  gulp.src(tasks.css.src)
+    .pipe(plumber({
+      errorHandler(err) {
+        util.log(err);
+        browserSync.notify(buildErrorMessage('css'));
+        this.emit('end');
+
+        /* Throw error in production builds to stop npm test/deploy scripts */
+        if (env === 'production') {
+          throw err;
+        }
+      },
+    }))
+    .pipe(sourcemaps.init())
+    .pipe(gulpif(
+      env === 'production',
+      sass(Object.assign(tasks.css.sass, {
+        outputStyle: 'compressed',
+      })),
+      sass(Object.assign(tasks.css.sass), {
+        outputStyle: 'expanded',
+      })
+    ))
+    .pipe(autoprefixer())
+    .pipe(rename({
+      basename: 'style',
+    }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(tasks.css.dest))
+    .pipe(browserSync.stream({
+      match: '**/*.css',
+    }))
+);
+
+/**
  * Docs Task
  *
  * 1. Deletes any previous files in the built docs folder
- * 2. Locates the src of docs specified in paths object
- * 3. Copies selected docs to the destination specified in the paths object
+ * 2. Locates the src of docs specified in tasks object
+ * 3. Copies selected docs to the destination specified in the tasks object
  */
 gulp.task('clean:docs', () =>
-  del(paths.docs.dest)
+  del(tasks.docs.dest)
 );
 
 gulp.task('docs', ['clean:docs'], () =>
-  gulp.src(paths.docs.src)
+  gulp.src(tasks.docs.src)
     .pipe(plumber({
       errorHandler(err) {
         util.log(err);
@@ -152,7 +213,7 @@ gulp.task('docs', ['clean:docs'], () =>
         }
       },
     }))
-    .pipe(gulp.dest(paths.docs.dest))
+    .pipe(gulp.dest(tasks.docs.dest))
     .pipe(browserSync.stream())
 );
 
@@ -160,16 +221,16 @@ gulp.task('docs', ['clean:docs'], () =>
  * Favicon Task
  *
  * 1. Deletes any previous files in the build folder
- * 2. Locates the src of favicon files specified in paths object
+ * 2. Locates the src of favicon files specified in tasks object
  * 3. Flattens the directory structure
- * 4. Copies favicon files to the destination specified in the paths object
+ * 4. Copies favicon files to the destination specified in the tasks object
  */
 gulp.task('clean:favicon', () =>
-  del(paths.favicon.manifest)
+  del(tasks.favicon.manifest)
 );
 
 gulp.task('favicon', ['clean:favicon'], () =>
-  gulp.src(paths.favicon.src)
+  gulp.src(tasks.favicon.src)
     .pipe(plumber({
       errorHandler(err) {
         util.log(err);
@@ -182,7 +243,7 @@ gulp.task('favicon', ['clean:favicon'], () =>
         }
       },
     }))
-    .pipe(gulp.dest(paths.favicon.dest))
+    .pipe(gulp.dest(tasks.favicon.dest))
     .pipe(browserSync.stream())
 );
 
@@ -190,16 +251,16 @@ gulp.task('favicon', ['clean:favicon'], () =>
  * Images Task
  *
  * 1. Deletes any previous files in the built img folder
- * 2. Locates the src of img files specified in paths object
+ * 2. Locates the src of img files specified in tasks object
  * 3. Flattens the directory structure
- * 4. Copies img files to the destination specified in the paths object
+ * 4. Copies img files to the destination specified in the tasks object
  */
 gulp.task('clean:img', () =>
-  del(paths.img.dest)
+  del(tasks.img.dest)
 );
 
 gulp.task('img', ['clean:img'], () =>
-  gulp.src(paths.img.src)
+  gulp.src(tasks.img.src)
     .pipe(plumber({
       errorHandler(err) {
         util.log(err);
@@ -213,43 +274,8 @@ gulp.task('img', ['clean:img'], () =>
       },
     }))
     .pipe(flatten())
-    .pipe(gulp.dest(paths.img.dest))
+    .pipe(gulp.dest(tasks.img.dest))
     .pipe(browserSync.stream())
-);
-
-/**
- * JavaScript Task
- *
- * 1. Deletes any previous files in the built js folder
- * 2. Locates the src of js files specified in paths object
- * 3. Initializes sourcemaps
- * 4. Concatenates all js files into one file called bundle.js
- * 5. Minifies the file if this is a production run
- * 6. Writes the file to the js destination specified in the paths object w/ sourcemap
- */
-gulp.task('js', () =>
-  gulp.src(paths.js.src)
-    .pipe(plumber({
-      errorHandler(err) {
-        util.log(err);
-        browserSync.notify(buildErrorMessage('js'));
-        this.emit('end');
-
-        /* Throw error in production builds to stop npm test/deploy scripts */
-        if (env === 'production') {
-          throw err;
-        }
-      },
-    }))
-    .pipe(sourcemaps.init())
-    .pipe(concat('app.js'))
-    .pipe(babel())
-    .pipe(gulpif(env === 'production', uglify()))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(paths.js.dest))
-    .pipe(browserSync.stream({
-      match: '**/*.js',
-    }))
 );
 
 /**
@@ -262,7 +288,7 @@ gulp.task('js', () =>
 gulp.task('jekyll', (done) =>
   childProcess
     .spawn('jekyll', ['build'], {
-      cwd: paths.jekyll.src,
+      cwd: tasks.jekyll.src,
       stdio: 'inherit',
     })
     .on('close', (code) => {
@@ -283,17 +309,52 @@ gulp.task('jekyll', (done) =>
 );
 
 /**
+ * JavaScript Task
+ *
+ * 1. Deletes any previous files in the built js folder
+ * 2. Locates the src of js files specified in tasks object
+ * 3. Initializes sourcemaps
+ * 4. Concatenates all js files into one file called bundle.js
+ * 5. Minifies the file if this is a production run
+ * 6. Writes the file to the js destination specified in the tasks object w/ sourcemap
+ */
+gulp.task('js', () =>
+  gulp.src(tasks.js.src)
+    .pipe(plumber({
+      errorHandler(err) {
+        util.log(err);
+        browserSync.notify(buildErrorMessage('js'));
+        this.emit('end');
+
+        /* Throw error in production builds to stop npm test/deploy scripts */
+        if (env === 'production') {
+          throw err;
+        }
+      },
+    }))
+    .pipe(sourcemaps.init())
+    .pipe(concat('app.js'))
+    .pipe(babel())
+    .pipe(gulpif(env === 'production', uglify()))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(tasks.js.dest))
+    .pipe(browserSync.stream({
+      match: '**/*.js',
+    }))
+);
+
+/**
  * Stylesheets Task
  *
- * 1. Locates the src of css files specified in paths object
+ * 1. Locates the src of css files specified in tasks object
  * 2. Initializes sourcemaps
  * 3. Minifies the file if this is a production run, otherwise leaves expanded
  * 4. Uses Autoprefixer to add vendor prefixes
  * 5. Renames file to style.css
- * 6. Writes the file to the css destination specified in the paths object w/ sourcemap
+ * 6. Writes the file to the css destination specified in the tasks object w/ sourcemap
  */
 gulp.task('css', () =>
-  gulp.src(paths.css.src)
+  gulp.src(tasks.css.src)
     .pipe(plumber({
       errorHandler(err) {
         util.log(err);
@@ -308,20 +369,20 @@ gulp.task('css', () =>
     }))
     .pipe(sourcemaps.init())
     .pipe(gulpif(
-        env === 'production',
-        sass({
-          outputStyle: 'compressed',
-        }),
-        sass({
-          outputStyle: 'expanded',
-        })
+      env === 'production',
+      sass(Object.assign(tasks.css.sass, {
+        outputStyle: 'compressed',
+      })),
+      sass(Object.assign(tasks.css.sass), {
+        outputStyle: 'expanded',
+      })
     ))
     .pipe(autoprefixer())
     .pipe(rename({
       basename: 'style',
     }))
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(paths.css.dest))
+    .pipe(gulp.dest(tasks.css.dest))
     .pipe(browserSync.stream({
       match: '**/*.css',
     }))
@@ -331,16 +392,16 @@ gulp.task('css', () =>
  * Vendor Fonts Task
  *
  * 1. Deletes all fonts in the destination directory
- * 2. Locates the src of vendor fonts specified in paths object
+ * 2. Locates the src of vendor fonts specified in tasks object
  * 3. Flattens the folder structure to one folder
- * 4. Copies flattened structure to fonts destination specified in the paths object
+ * 4. Copies flattened structure to fonts destination specified in the tasks object
  */
 gulp.task('clean:fonts', () =>
-  del(paths.vendor.fonts.dest)
+  del(tasks.vendor.fonts.dest)
 );
 
 gulp.task('vendor:fonts', ['clean:fonts'], () =>
-  gulp.src(paths.vendor.fonts.src)
+  gulp.src(tasks.vendor.fonts.src)
     .pipe(plumber({
       errorHandler(err) {
         util.log(err);
@@ -354,20 +415,59 @@ gulp.task('vendor:fonts', ['clean:fonts'], () =>
       },
     }))
     .pipe(flatten())
-    .pipe(gulp.dest(paths.vendor.fonts.dest))
+    .pipe(gulp.dest(tasks.vendor.fonts.dest))
+    .pipe(browserSync.stream())
+);
+
+/**
+ * Vendor Stylesheets Task
+ *
+ * 1. Locates the src of vendor css files specified in tasks object
+ * 2. Minifies the file if this is a production run, otherwise leaves expanded
+ * 3. Uses Autoprefixer to add vendor prefixes
+ * 4. Writes the file to the vendor css destination specified in the tasks object
+ */
+gulp.task('vendor:css', () =>
+  gulp.src(tasks.vendor.css.src)
+    .pipe(plumber({
+      errorHandler(err) {
+        util.log(err);
+        browserSync.notify(buildErrorMessage('vendor:css'));
+        this.emit('end');
+
+        /* Throw error in production builds to stop npm test/deploy scripts */
+        if (env === 'production') {
+          throw err;
+        }
+      },
+    }))
+    .pipe(sourcemaps.init())
+    .pipe(concat('vendor.css'))
+    .pipe(gulpif(
+      env === 'production',
+      sass({
+        outputStyle: 'compressed',
+      }),
+      sass({
+        outputStyle: 'expanded',
+      })
+    ))
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(tasks.vendor.css.dest))
     .pipe(browserSync.stream())
 );
 
 /**
  * Vendor JavaScript Task
  *
- * 1. Locates the src of vendor js files specified in paths object
+ * 1. Locates the src of vendor js files specified in tasks object
  * 2. Concatenates all vendor js files into one file called vendor.js
  * 3. Minifies the file if this is a production run
- * 4. Writes the file to the vendor js destination specified in the paths object
+ * 4. Writes the file to the vendor js destination specified in the tasks object
  */
 gulp.task('vendor:js', () =>
-  gulp.src(paths.vendor.js.src)
+  gulp.src(tasks.vendor.js.src)
     .pipe(plumber({
       errorHandler(err) {
         util.log(err);
@@ -384,45 +484,7 @@ gulp.task('vendor:js', () =>
     .pipe(concat('vendor.js'))
     .pipe(gulpif(env === 'production', uglify()))
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(paths.vendor.js.dest))
-    .pipe(browserSync.stream())
-);
-
-/**
- * Vendor Stylesheets Task
- *
- * 1. Locates the src of vendor css files specified in paths object
- * 2. Minifies the file if this is a production run, otherwise leaves expanded
- * 3. Uses Autoprefixer to add vendor prefixes
- * 4. Writes the file to the vendor css destination specified in the paths object
- */
-gulp.task('vendor:css', () =>
-  gulp.src(paths.vendor.css.src)
-    .pipe(plumber({
-      errorHandler(err) {
-        util.log(err);
-        browserSync.notify(buildErrorMessage('vendor:css'));
-        this.emit('end');
-
-        /* Throw error in production builds to stop npm test/deploy scripts */
-        if (env === 'production') {
-          throw err;
-        }
-      },
-    }))
-    .pipe(sourcemaps.init())
-    .pipe(gulpif(
-      env === 'production',
-      sass({
-        outputStyle: 'compressed',
-      }),
-      sass({
-        outputStyle: 'expanded',
-      })
-    ))
-    .pipe(autoprefixer())
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(paths.vendor.css.dest))
+    .pipe(gulp.dest(tasks.vendor.js.dest))
     .pipe(browserSync.stream())
 );
 
@@ -459,15 +521,15 @@ gulp.task('serve', ['build'], () => {
     },
   });
 
-  gulp.watch(paths.docs.watchFiles, ['docs']);
-  gulp.watch(paths.favicon.watchFiles, ['favicon']);
-  gulp.watch(paths.img.watchFiles, ['img']);
-  gulp.watch(paths.js.watchFiles, ['js']);
-  gulp.watch(paths.jekyll.watchFiles, ['build'], browserSync.reload);
-  gulp.watch(paths.css.watchFiles, ['css']);
-  gulp.watch(paths.vendor.fonts.watchFiles, ['vendor:fonts']);
-  gulp.watch(paths.vendor.js.watchFiles, ['vendor:js']);
-  gulp.watch(paths.vendor.css.watchFiles, ['vendor:css']);
+  gulp.watch(tasks.css.watchFiles, ['css']);
+  gulp.watch(tasks.docs.watchFiles, ['docs']);
+  gulp.watch(tasks.favicon.watchFiles, ['favicon']);
+  gulp.watch(tasks.img.watchFiles, ['img']);
+  gulp.watch(tasks.jekyll.watchFiles, ['build'], browserSync.reload);
+  gulp.watch(tasks.js.watchFiles, ['js']);
+  gulp.watch(tasks.vendor.fonts.watchFiles, ['vendor:fonts']);
+  gulp.watch(tasks.vendor.css.watchFiles, ['vendor:css']);
+  gulp.watch(tasks.vendor.js.watchFiles, ['vendor:js']);
 });
 
 /**
