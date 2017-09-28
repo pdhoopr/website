caffa81febfe52bb30b3acec31c114420916bccb
Patrick Hooper <patrick.d.hooper@gmail.com>
2017-09-28 18:13:42 -0400

    Choose lighter gray for theme and update code, favicon, and resume

--- public/index.jsx
+++ public/index.jsx
@@ -21,8 +21,9 @@ export default function renderHtml(data, body) {
             .filter(asset => asset.match(/\.css(\.map)?$/))
             .map(stylesheet => <link href={`/${stylesheet}`} rel="stylesheet" />)
           }
-          <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="16x16 32x32 48x48" href="/favicon.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
           <link rel="manifest" href="/manifest.json" />
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#fa755a" />
           <meta name="theme-color" content="#ffffff" />
