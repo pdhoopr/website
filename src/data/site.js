83d55f762630087f1d99f94a311488ca6d386af8
Patrick Hooper <patrick.d.hooper@gmail.com>
2020-02-13 12:04:17 -0500

    Rewrite bio

--- src/data/site.js
+++ src/data/site.js
@@ -4,6 +4,6 @@ export default {
   title: 'Patrick Hooper',
   shortTitle: 'PDH',
   description:
-    "Web (app) developer, life enthusiast, University of Michigan alumnus, and all-around neat guy living in Ann Arbor, MI. Also, Marie Hooper's biggest fan.",
+    "Web (app) developer, life enthusiast, national park junkie, and all-around neat guy. Probably Marie Hooper's biggest fan. Ann Arbor, Michigan.",
   siteUrl: homepage,
 }
