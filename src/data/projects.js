fe8859fa0d3f5166b37eff1132c54c11a13a09ef
Patrick Hooper <patrick.d.hooper@gmail.com>
2020-04-06 09:14:52 -0400

    Clean up projects list

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,36 +1,16 @@
 export default [
   {
-    name: 'Codework',
-    description:
-      "A programming challenge app where users can add challenges, submit solutions, and view friends' submissions; all in realtime.",
-    site: 'https://codework.netlify.com/',
-    repo: 'https://github.com/letscodework/codework',
-  },
-  {
     name: 'GPS to GPX',
     description:
       'A simple library that formats GPS waypoint data from an activity (a run, bike ride, hike, etc.) as GPX.',
+    site: 'https://npm.im/gps-to-gpx',
     repo: 'https://github.com/pdhoopr/gps-to-gpx',
   },
   {
-    name: 'WGI.buzz',
+    name: 'Codework',
     description:
-      'A sandbox app for the WGI think tank to try out cutting-edge business intelligence ideas.',
-    site: 'https://wgibuzz.netlify.com',
-    repo: 'https://github.com/pdhoopr/wgi.buzz',
-  },
-  {
-    name: 'Jahleh + Patrick',
-    description:
-      'The wedding website for me and my beautiful wife, made with love and a lot of ice cream.',
-    site: 'https://jahlehandpatrick.netlify.com',
-    repo: 'https://github.com/pdhoopr/jahleh-and-patrick',
-  },
-  {
-    name: "Ms. O'Meara's Multiage",
-    description:
-      "The whimsical classroom website for Ms. O'Meara (my mom), her students, and their parents.",
-    site: 'https://msomearasmultiage.netlify.com/',
-    repo: 'https://github.com/pdhoopr/ms-omearas-multiage',
+      "A programming challenge app where users can add challenges, submit solutions, and view friends' submissions; all in realtime.",
+    site: 'https://codework.coolthings.dev',
+    repo: 'https://github.com/mariehooper/codework',
   },
 ];
