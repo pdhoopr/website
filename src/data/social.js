a9c380f269c1382cebdba36f289aa15f83bd6e7d
Patrick Hooper <patrick.d.hooper@gmail.com>
2020-07-19 12:42:08 -0400

    Remove Dev.to and Twitter icons

--- src/data/social.js
+++ src/data/social.js
@@ -1,24 +1,12 @@
-import { ReactComponent as DevIcon } from '../images/dev-community.svg';
 import { ReactComponent as GithubIcon } from '../images/github.svg';
 import { ReactComponent as InstagramIcon } from '../images/instagram.svg';
 import { ReactComponent as LinkedinIcon } from '../images/linkedin.svg';
-import { ReactComponent as TwitterIcon } from '../images/twitter.svg';
 
 export default [
   {
     icon: GithubIcon,
     link: 'https://github.com/pdhoopr',
     title: 'GitHub',
-  },
-  {
-    icon: TwitterIcon,
-    link: 'https://twitter.com/pdhoopr',
-    title: 'Twitter',
-  },
-  {
-    icon: DevIcon,
-    link: 'https://dev.to/pdhoopr',
-    title: 'DEV Community',
   },
   {
     icon: LinkedinIcon,
